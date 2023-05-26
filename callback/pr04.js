6e39d090d6dc7a0d2bc2dec67aa8967cfd9b6e30
Kshitija Yamgar <98943820+kshitijayamgar@users.noreply.github.com>
2023-05-26 12:15:04 +0530

    pr04.js

---
+++ callback/pr04.js
@@ -0,0 +1,16 @@
+function even(num) {
+    if(num%2==0){
+      console.log("num is even");
+    }
+    else{
+        console.log("num is odd");
+    }
+  }
+  
+  function simple(xyz) {
+   var num=  parseInt(prompt("enter the number"));
+    xyz(num);
+  }
+  
+  simple(even);
+  
